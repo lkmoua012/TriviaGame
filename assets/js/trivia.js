6e50d2a37466225b883af837aedb932c88cbb5ba
Moua Larry <lkmoua@gmail.com>
2018-07-11 03:09:05 -0700

    version 1

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -6,7 +6,7 @@ const myQuestions =[
             a: "Tyrant",
             b: "Wesker",
             c: "Nemesis",
-            d: "LasPlagas"
+            d: "Las Plagas"
         },
         correct: "a"
     },
@@ -33,54 +33,179 @@ const myQuestions =[
         correct: "b"
     },
 
+    {
+        question: "Where does Resident Evil Revelations take place on the timeline?",
+        answers: {
+            a: "5 and 6",
+            b: "3 and 4",
+            c: "4 and 5",
+            d: "6 and 7"
+        },
+        correct: "c"
+    },
+
+    {
+        question: "How many Resident Evil games does Claire Redfield star in?",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3",
+            d: "4"
+        },
+        correct: "c"
+    },
+
+    {
+        question: "Where does Resident Evil 4 take place?",
+        answers: {
+            a: "Spain",
+            b: "England",
+            c: "Raccoon City",
+            d: "Shanghai"
+        },
+        correct: "a"
+    },
+
+    {
+        question: "What is the first virus created by Umbrella Corporation?",
+        answers: {
+            a: "G-Virus",
+            b: "T-Virus",
+            c: "C-Virus",
+            d: "Uroboros"
+        },
+        correct: "b"
+    },
+
+    {
+        question: "Who is the main character in Resident Evil 7?",
+        answers: {
+            a: "Mia",
+            b: "Leon",
+            c: "Jake",
+            d: "Ethan"
+        },
+        correct: "d"
+    },
+
+    {
+        question: "What is the name of the mansion in Resident Evil 1?",
+        answers: {
+            a: "Arklay Mansion",
+            b: "Spencer Mansion",
+            c: "Nemesis Mansion",
+            d: "Hive Mansion"
+        },
+        correct: "b"
+    },
+
+    {
+        question: "What is the name of the spin-off that takes place during the Raccoon City incident?",
+        answers: {
+            a: "Umbrella Chronicles",
+            b: "Gaiden",
+            c: "Dead Aim",
+            d: "Outbreak"
+        },
+        correct: "d"
+    },
+
 ];
 
 
 $(document).ready(function() {
 
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
+    // AUDIO AND GLOBAL VARIABLES
+    var bgm = new Audio("assets/audio/re2_save.mp3");
+    var gameover = new Audio("assets/audio/r_evil.wav");
+    var countdown = 31;
+    var intervalID = setInterval(timer, 1000);
 
-function buildQuiz(){}
+    // FUNCTIONS
+    document.addEventListener("click", function(){
+        bgm.play();
+    });
 
-    for (i = 0; i < myQuestions.length; i++){
+    function timer(){
 
-        var quizHTML = "<h3>" + myQuestions[i].question + "</h3>"
-        var answerHTML = "<div class='answers'><input type='radio' name ='answer"+[i]+"' value='a'>" + myQuestions[i].answers.a +
-        "<br> <input type='radio' name ='answer"+i+"' value='b'>" + myQuestions[i].answers.b +
-        "<br> <input type='radio' name ='answer"+i+"' value='c'>" + myQuestions[i].answers.c +
-        "<br> <input type='radio' name ='answer"+i+"' value='d'>" + myQuestions[i].answers.d + "</div>"
+        countdown--;
+        $("#timer").html("<h2>" + countdown + "</h2>");
 
-        console.log(myQuestions[i].question);
-        console.log(myQuestions[i].correct);
-        console.log(answerHTML);
-        $("#quiz").append(quizHTML, answerHTML);
+        if (countdown === 0) {
 
-    };
-//end of buildQuiz
+            showResults();
 
-function showResults(){
-
-    var numCorrect = 0;
-
-    for (var i = 0; i < myQuestions.length; i++){
-
-        var userAnswer = $( "input[type=radio][name='answer"+i+"']:checked" ).val();
-
-        if (userAnswer === myQuestions[i].correct){
-            numCorrect++;
-        }
+        };
 
     };
+    // end of timer
 
-    $("#results").text(numCorrect + " out of " + myQuestions.length);
+    function buildQuiz(){
 
-};
-//end of showResults
+        for (i = 0; i < myQuestions.length; i++){
 
-buildQuiz();
+            var quizHTML = "<h3>" + myQuestions[i].question + "</h3>"
+            var answerHTML = "<span class='answers'><input type='radio' name ='answer"+[i]+"' value='a'>" + myQuestions[i].answers.a +
+            "<input type='radio' name ='answer"+i+"' value='b'>" + myQuestions[i].answers.b +
+            "<input type='radio' name ='answer"+i+"' value='c'>" + myQuestions[i].answers.c +
+            "<input type='radio' name ='answer"+i+"' value='d'>" + myQuestions[i].answers.d + "</span> <br> <br>"
 
-$("#submit").click(showResults);
+            $("#quiz").append(quizHTML, answerHTML);
 
-// End of Document Ready
+        };
+        // end of for loop
+
+    };
+    //end of buildQuiz
+
+    function showResults(){
+
+        clearInterval(intervalID);
+
+        gameover.play();
+
+        var numCorrect = 0;
+        var numIncorrect = 0;
+        var noAnswer = 0;
+
+        for (var i = 0; i < myQuestions.length; i++){
+
+            var userAnswer = $( "input[type=radio][name='answer"+i+"']:checked" ).val();
+
+            if (userAnswer === myQuestions[i].correct){
+                numCorrect++;
+            } else {
+                numIncorrect++;
+            };
+
+            if (!(userAnswer)){
+                noAnswer++;
+            }
+
+        };
+
+        $("#timer").text("Refresh to try again!");
+
+        $("#quiz").remove();
+
+        $("#submit").remove();
+
+        var resultsHTML = "<p>Correct Answers: " + numCorrect + "</p>"
+        + "<p>Incorrect Answers: " + numIncorrect + "</p>"
+        + "<p>Unanswered Questions: " + noAnswer + "</p>";
+
+        $("#results").html(resultsHTML);
+
+    };
+    //end of showResults
+
+    // CALL FUNCTIONS AND EVENTS
+
+    timer();
+
+    buildQuiz();
+
+    $("#submit").click(showResults);
+
+    // End of Document Ready
 });
